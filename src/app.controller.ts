3e9c117f5189ca0b8444456c8d9a1caff428feeb
Yuto Ono <stellarwindmaster@gmail.com>
2022-08-22 19:18:23 +0900

    api/hello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/api/hello')
+  apiHello(): string[] {
+    return ['Hello', 'World!'];
+  }
 }
